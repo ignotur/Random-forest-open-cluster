c51380e6ad9cf525d1a6311b215205f5a634b73c
glukhove <ernest_glukhov@mail.ru>
2020-09-02 10:54:19 +0300

    added random split, calculation of accuracy and confusion matrix

--- random_forest.py
+++ random_forest.py
@@ -1,5 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
 from math import *
 import numpy as np
 import matplotlib as mpl
@@ -16,11 +18,13 @@ false_mtr = np.load ('matrix_false.npy')
 type1 = np.ones (len(real_mtr))
 type2 = np.zeros (len(false_mtr))
 
-print (len(real_mtr[0: -5]))
-print (len(false_mtr[0: -5]))
+print (len(real_mtr))
+print (len(false_mtr))
 
-mtr = np.concatenate((real_mtr[0: -5], false_mtr[0: -5]))
-tp  = np.concatenate((type1[0: -5], type2[0: -5]))
+mtr = np.concatenate((real_mtr, false_mtr))
+tp  = np.concatenate((type1, type2))
+
+x_train, x_test, y_train, y_test = train_test_split(mtr, tp)
 
 
 #plt.imshow(real_mtr[0].reshape((20,20)), interpolation='nearest')
@@ -33,15 +37,12 @@ tp  = np.concatenate((type1[0: -5], type2[0: -5]))
 
 #	print (tp[i])
 
-n_smp = len(tp)
+n_smp = len(x_train)
 
 clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(mtr, tp)
+clf.fit(x_train, y_train)
 
+y_pred = clf.predict(x_test)
 
-#for i in range (1, 5):
-
-print ('Real:  ', clf.predict(real_mtr[-5:-1]))
-print ('False: ', clf.predict(false_mtr[-5:-1]))
-
-
+print(accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
